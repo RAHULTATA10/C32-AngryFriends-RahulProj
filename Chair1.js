41d211be86850b083641b02c185643109537eb16
RAHUL <56194955+RAHULTATA10@users.noreply.github.com>
2020-05-30 22:27:20 +0530

    Update Chair1.js

--- Chair1.js
+++ Chair1.js
@@ -3,5 +3,24 @@ class Chair1 extends BaseClass {
       super(x,y,width,height);
       this.image = loadImage("sprites/eechair.png");
     }
-  
-  };
+   display(){
+   //console.log(this.body.speed);
+   if(this.body.speed < 3){
+    super.display();
+   }
+   else{
+     World.remove(world, this.body);
+     push();
+     this.Visiblity = this.Visiblity - 5;
+     tint(255,this.Visiblity);
+     image(this.image, this.body.position.x, this.body.position.y, 50, 50);
+     pop();
+   }
+   
+ }
+    score(){
+      if (this.Visiblity < 0 && this.Visiblity > -1005){
+         score++;
+      }
+   }
+ };
