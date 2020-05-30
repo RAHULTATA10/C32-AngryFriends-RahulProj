d2fe25346ce28d67240ccc74bb5aca7fb6da7a91
RAHUL <56194955+RAHULTATA10@users.noreply.github.com>
2020-05-30 22:47:31 +0530

    Update BaseClass.js

--- BaseClass.js
+++ BaseClass.js
@@ -1,9 +1,9 @@
 class BaseClass{
     constructor(x, y, width, height, angle) {
         var options = {
-            'restitution':8,
-            'friction':10,
-            'density':10
+            'restitution':0.8,
+            'friction':1,
+            'density':1
         }
         this.body = Bodies.rectangle(x, y, width, height, options);
         this.width = width;
