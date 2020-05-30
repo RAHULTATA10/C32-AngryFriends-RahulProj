48456a51e233adfce0eeef28e5e966d3d0a262cd
RAHUL <56194955+RAHULTATA10@users.noreply.github.com>
2020-05-30 22:06:06 +0530

    Update Boy5.js

--- Boy5.js
+++ Boy5.js
@@ -4,6 +4,21 @@ class Boy5 extends BaseClass {
       this.image = loadImage("sprites/boy3.png");
       this.Visiblity = 255;
     }
+    display(){
+   //console.log(this.body.speed);
+   if(this.body.speed < 3){
+    super.display();
+   }
+   else{
+     World.remove(world, this.body);
+     push();
+     this.Visiblity = this.Visiblity - 5;
+     tint(255,this.Visiblity);
+     image(this.image, this.body.position.x, this.body.position.y, 50, 50);
+     pop();
+   }
+   
+ }
     score(){
       if (this.Visiblity < 0 && this.Visiblity > -1205){
          score++;
